8d05ff896fd8811623af3c57af56229f76d48aeb
yan_cy <yan_cy@Ctrip.com>
2019-07-30 00:01:57 +0800

    新增two-sum

--- src/two-sum.js
+++ src/two-sum.js
@@ -6,3 +6,59 @@
     因为 nums[0] + nums[1] = 2 + 7 = 9
     所以返回 [0, 1]
  */
+
+var twoSum1 = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            } 
+        }
+    }
+}
+
+// twoSum1，最传统的方法，两次循环，执行耗时156ms，内存占用34.7MB
+
+var twoSum2 = function(nums, target) {
+    let obj = {};
+    for (let i = 0; i < nums.length; i++) {
+        let n = target - nums[i];
+        if (obj.hasOwnProperty(n)) {
+            return [obj[n], i]
+        }
+        obj[nums[i]] = i
+    }
+    return []
+}
+
+// twoSum2，将数组元素转为对象的key，然后下标作为value，利用对象存储值更快，执行耗时96ms，内存占用34.1MB
+
+var twoSum3 = function(nums, target) {
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let n = target - nums[i];
+        if (map.has(n)) {
+            return [map.get(n), i]
+        }
+        map.set(nums[i], i)
+    }
+    return []
+}
+
+// twoSum3，原理同上，但是利用es6的Map数据结构，存储读值更快，执行耗时64ms，内存占用35MB
+
+var twoSum4 = function(nums, target) {
+    let map = new Map(),
+        l = nums.length;
+    for(let i = 0; i<l; i++) {
+        let val = nums[i],
+            n = target - val;
+        if (map.has(n)) {
+            return [map.get(n), i]
+        }
+        map.set(val, i)
+    }
+    return []
+}
+
+// 尽可能的优化一下
